d0c374a1969564a934fb43c3a768c9922ecbe971
AitorMelero <meleropiconaitor@gmail.com>
2024-06-05 18:22:39 +0200

    fix: Corregir error de estilos de posicionamiento de los datos del contenedor de video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -13,15 +13,17 @@ export const Video: React.FC<Props> = ({ videoData }) => {
       <img src={imageUrl} className="card-img-top video-card__image" alt="Video Image" />
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
-        <p className="card-text">
-          <small className="video-card__text">{md5String}</small>
-        </p>
-        <p className="card-text">
-          <small className="video-card__text">{visits}</small>
-        </p>
-        <p className="card-text">
-          <small className="video-card__text">{videoDate}</small>
-        </p>
+        <div className="card-text__container">
+          <p className="card-text">
+            <small className="video-card__text">{md5String}</small>
+          </p>
+          <p className="card-text">
+            <small className="video-card__text">{visits}</small>
+          </p>
+          <p className="card-text">
+            <small className="video-card__text">{videoDate}</small>
+          </p>
+        </div>
       </div>
     </div>
   )
